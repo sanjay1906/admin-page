24ef674103128ed6726b0b9dbf1c28ec0bf59088
Sanjay 1906 <sanjaythakur@venom.com>
2019-07-04 21:55:22 +0530

    userfeadback

--- javascritt/java.js
+++ javascritt/java.js
@@ -1,6 +1,46 @@
-var check = document.getElementById('open');
-function open()
-{
-    check.style.display = "block";
 
-}
+            /*dropdown*/
+
+$('.dropdown-trigger').dropdown({
+    coverTrigger: false, 
+    hover: true,
+    autoTrigger: true,
+    alignment: 'right',
+    constrainWidth: true,
+    container: null,
+    id:'left',
+    isScrollable:true,
+    focusedIndex:200,
+    isOpan:true,
+    closeOnClick: true,
+    inDuration: 300,
+    outDuration:225,
+    onOpenStart: null,
+    onOpenEnd: null,
+    onCloseStart: null,
+    onCloseEnd: null
+  }
+);
+
+
+       /*sidenav*/
+
+$('.sidenav').sidenav({
+    edge: 'left',
+    draggable: false,
+    inDuration:0,
+    outDuration:1000,
+    onOpenStart:null,
+    onOpenEnd:null, 
+    onCloseStart:null,
+    onCloseEnd:null,
+    preventScrolling:true,
+    isDragged:true,
+    isOpen:false,
+
+});
+        
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+       
